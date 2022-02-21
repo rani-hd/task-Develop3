c1377a963d091b1aa6fc2e063787b2182888c2e8
Angular <Rani@hudsondata.com>
2022-02-21 00:24:43 -0700

    code updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,19 @@
 export const environment = {
-  firebase: {
-    projectId: 'task2-98125',
-    appId: '1:601748915865:web:362417b33d981330ff1a08',
-    storageBucket: 'task2-98125.appspot.com',
-    apiKey: 'AIzaSyAKUkd3opkwHTI87PFIb3ZQMul7_Ud4H_4',
-    authDomain: 'task2-98125.firebaseapp.com',
-    messagingSenderId: '601748915865',
-  },
+   firebase: {
+  apiKey: "AIzaSyAKUkd3opkwHTI87PFIb3ZQMul7_Ud4H_4",
+  authDomain: "task2-98125.firebaseapp.com",
+  databaseURL: "https://task2-98125-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "task2-98125",
+  storageBucket: "task2-98125.appspot.com",
+  messagingSenderId: "601748915865",
+  appId: "1:601748915865:web:362417b33d981330ff1a08"
+}, //{
+  //   projectId: 'task2-98125',
+  //   appId: '1:601748915865:web:362417b33d981330ff1a08',
+  //   storageBucket: 'task2-98125.appspot.com',
+  //   apiKey: 'AIzaSyAKUkd3opkwHTI87PFIb3ZQMul7_Ud4H_4',
+  //   authDomain: 'task2-98125.firebaseapp.com',
+  //   messagingSenderId: '601748915865',
+  // },
   production: true
 };
